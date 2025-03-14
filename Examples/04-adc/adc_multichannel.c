09f8afb155a967eab4d7672c4c3bd824d8907419
fhermosilloup <fhermosillo@up.edu.mx>
2025-03-14 10:29:43 -0600

    Create adc_multichannel.c

---
+++ Examples/04-adc/adc_multichannel.c
@@ -0,0 +1,23 @@
+uint16_t ADC_Read(uint32_t rank, uint32_t channel)
+{
+	/* Configure channel */
+	ADC_ChannelConfTypeDef sConfig = {0};
+	sConfig.Channel = channel;
+	sConfig.Rank = rank;
+	sConfig.SamplingTime = ADC_SAMPLETIME_810CYCLES_5;
+	sConfig.SingleDiff = ADC_SINGLE_ENDED;
+	sConfig.OffsetNumber = ADC_OFFSET_NONE;
+	sConfig.Offset = 0;
+	sConfig.OffsetSignedSaturation = DISABLE;
+	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/* Convert the Channel */
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion(&hadc1, 100);
+	uint16_t adcval = HAL_ADC_GetValue(&hadc1);
+	HAL_ADC_Stop(&hadc1);
+	return adcval;
+}
